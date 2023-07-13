4346924883c453c7ee02e6bcd3240b822b0a944d
Jean Furman <jean_11spfc@hotmail.com>
2023-07-12 23:45:19 -0300

    Criando repositório próprio para transações

---
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -0,0 +1,16 @@
+package br.com.banco.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.banco.model.Conta;
+import br.com.banco.model.Transferencia;
+
+
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Long>{
+	List<Transferencia> findByContaId(Conta contaId);
+	List<Transferencia> findByNomeOperadorTransacao(String nomeOperadorTransacao);
+	
+	
+}
