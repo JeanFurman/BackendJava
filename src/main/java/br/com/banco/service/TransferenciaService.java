fe413fe3c7c848e2fa7549f3047e5b910b8e7b50
Jean Furman <jean_11spfc@hotmail.com>
2023-07-12 23:51:33 -0300

    Criando classe transferencia service e escrevendo 3 métodos (listar todos, buscar por id da conta e buscar por nome do operador)

---
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -0,0 +1,37 @@
+package br.com.banco.service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.banco.model.Transferencia;
+import br.com.banco.repository.ContaRepository;
+import br.com.banco.repository.TransferenciaRepository;
+
+@Service
+public class TransferenciaService {
+	private final ContaRepository contaRepository;
+	private final TransferenciaRepository transferenciaRepository;
+
+	public TransferenciaService(ContaRepository contaRepository, TransferenciaRepository transferenciaRepository) {
+		this.contaRepository = contaRepository;
+		this.transferenciaRepository = transferenciaRepository;
+	}
+
+	public List<Transferencia> listarTodasAsTransferencias(){
+		return transferenciaRepository.findAll();
+	}
+	
+	public List<Transferencia> buscarTransferenciasPorIdConta(Long id) {
+		return contaRepository.findById(id)
+				.map(c -> transferenciaRepository.findByContaId(c))
+				.orElseThrow(() -> new RuntimeException("Conta not found"));
+	}
+	
+	public List<Transferencia> buscarTransferenciasPorNomeOperadorTransacao(String nome) {
+		return transferenciaRepository.findByNomeOperadorTransacao(nome);
+	}
+	
+}
