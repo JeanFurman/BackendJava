414b6bf9feb03637fa6775d3828fdf59b24c2da7
Jean Furman <jean_11spfc@hotmail.com>
2023-07-17 21:54:50 -0300

    Refatoração do controller e do service

--- src/main/java/br/com/banco/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -1,20 +1,14 @@
 package br.com.banco.controller;
 
-import java.time.LocalDateTime;
-import java.util.stream.Collectors;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.dto.PageDTO;
@@ -27,8 +21,7 @@ import br.com.banco.service.TransferenciaService;
 public class TransferenciaController {
 	
 	private final TransferenciaService transferenciaService;
-	private final String regexData = "^\\d{2}/\\d{2}/\\d{4}$";
-	private final String regexNome = "^[a-zA-Z ]{1,50}$";
+	
 	
 	public TransferenciaController(TransferenciaService transferenciaService) {
 		this.transferenciaService = transferenciaService;
@@ -47,41 +40,21 @@ public class TransferenciaController {
 			@RequestParam(required = false, defaultValue = "") String nome, 
 			@RequestParam(required = false, defaultValue = "") String dataInicio,
 			@RequestParam(required = false, defaultValue = "") String dataFim){
+		// Por nome do operador + período
 		if(!nome.isBlank() && !dataInicio.isBlank() & !dataFim.isBlank()) {
-			LocalDateTime dataI = transferenciaService.validaData(dataInicio, regexData);
-			LocalDateTime dataF = transferenciaService.validaData(dataFim, regexData);
-			String nomeValid = transferenciaService.validaNome(nome, regexNome);
-			PageDTO<Transferencia> pages = transferenciaService
-					.fazPaginas(pageable.getPageNumber(),
-			transferenciaService.buscarTransferenciasPorNomeOperadorTransacao(nomeValid)
-			.stream()
-			.filter(t -> t.getDataTransferencia().equals(dataI) || t.getDataTransferencia().isAfter(dataI))
-			.filter(t -> t.getDataTransferencia().equals(dataF) || t.getDataTransferencia().isBefore(dataF))
-			.collect(Collectors.toList()));
-			return pages;
+			return transferenciaService.buscarTransferenciasPorNomeOperadorTransacaoEPorPeriodo(pageable, dataInicio, dataFim, nome);
 		}
+		// Por nome do operador
 		if(!nome.isBlank() && dataInicio.isBlank() & dataFim.isBlank()) {
-			String nomeValid = transferenciaService.validaNome(nome, regexNome);
 			
-			PageDTO<Transferencia> pages = transferenciaService
-					.fazPaginas(pageable.getPageNumber(), transferenciaService.buscarTransferenciasPorNomeOperadorTransacao(nomeValid));
-			return pages;
+			return transferenciaService.buscarTransferenciasPorNomeOperadorTransacao(pageable, nome);
 		}
+		// Por período
 		if(nome.isBlank() && !dataInicio.isBlank() & !dataFim.isBlank()) {
-			LocalDateTime dataI = transferenciaService.validaData(dataInicio, regexData);
-			LocalDateTime dataF = transferenciaService.validaData(dataFim, regexData);
-			PageDTO<Transferencia> pages = transferenciaService
-					.fazPaginas(pageable.getPageNumber(),
-							transferenciaService.listarTodasAsTransferencias()
-							.stream()
-							.filter(t -> t.getDataTransferencia().equals(dataI) || t.getDataTransferencia().isAfter(dataI))
-							.filter(t -> t.getDataTransferencia().equals(dataF) || t.getDataTransferencia().isBefore(dataF))
-							.collect(Collectors.toList()));
-			return pages;
+			return transferenciaService.buscarTransferenciasPorPeriodo(pageable, dataInicio, dataFim);
 		}
-		PageDTO<Transferencia> pages = transferenciaService
-				.fazPaginas(pageable.getPageNumber(),transferenciaService.listarTodasAsTransferencias());
-		return pages;
+		// Todos
+		return transferenciaService.listarTodasAsTransferencias(pageable);
 		
 	}
 }
--- src/main/java/br/com/banco/service/TransferenciaService.java
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
@@ -22,14 +23,16 @@ import br.com.banco.repository.TransferenciaRepository;
 public class TransferenciaService {
 	private final ContaRepository contaRepository;
 	private final TransferenciaRepository transferenciaRepository;
+	private final String regexData = "^\\d{2}/\\d{2}/\\d{4}$";
+	private final String regexNome = "^[a-zA-Z ]{1,50}$";
 
 	public TransferenciaService(ContaRepository contaRepository, TransferenciaRepository transferenciaRepository) {
 		this.contaRepository = contaRepository;
 		this.transferenciaRepository = transferenciaRepository;
 	}
 
-	public List<Transferencia> listarTodasAsTransferencias(){
-		return transferenciaRepository.findAll();
+	public PageDTO<Transferencia> listarTodasAsTransferencias(Pageable pageable){
+		return fazPaginas(pageable.getPageNumber(), transferenciaRepository.findAll());
 	}
 	
 	public PageDTO<Transferencia> buscarTransferenciasPorIdConta(Pageable pageable, Long id) {
@@ -40,15 +43,38 @@ public class TransferenciaService {
 		
 	}
 	
-	public List<Transferencia> buscarTransferenciasPorNomeOperadorTransacao(String nome) {
-		List<Transferencia> l = transferenciaRepository.findByNomeOperadorTransacao(nome);
+	public PageDTO<Transferencia> buscarTransferenciasPorNomeOperadorTransacao(Pageable pageable, String nome){
+		return fazPaginas(pageable.getPageNumber(), listarPorNomeDoOperadorTransacao(nome));
+	}
+	
+	public PageDTO<Transferencia> buscarTransferenciasPorPeriodo(Pageable pageable, String dataInicio, String dataFim){	
+		return fazPaginas(pageable.getPageNumber(), listarPeriodo(dataInicio, dataFim, transferenciaRepository.findAll()));
+	}
+	
+	public PageDTO<Transferencia> buscarTransferenciasPorNomeOperadorTransacaoEPorPeriodo(Pageable pageable, String dataInicio, String dataFim, String nome){	
+		return fazPaginas(pageable.getPageNumber(), listarPeriodo(dataInicio, dataFim, listarPorNomeDoOperadorTransacao(nome)));
+	}
+	
+	private List<Transferencia> listarPorNomeDoOperadorTransacao(String nome) {
+		String nomeValid = validaNome(nome, regexNome);
+		List<Transferencia> l = transferenciaRepository.findByNomeOperadorTransacao(nomeValid);
 		if(l.size() < 1) {
 			throw new RegistroNotFoundException(nome);
 		}
 		return l;
 	}
 	
-	public PageDTO<Transferencia> fazPaginas(int pageNumber, List<Transferencia> lista){
+	private List<Transferencia> listarPeriodo(String dataInicio, String dataFim, List<Transferencia> lista){
+		LocalDateTime dataI = validaData(dataInicio, regexData);
+		LocalDateTime dataF = validaData(dataFim, regexData);
+		
+		return lista.stream()
+				.filter(t -> t.getDataTransferencia().equals(dataI) || t.getDataTransferencia().isAfter(dataI))
+				.filter(t -> t.getDataTransferencia().equals(dataF) || t.getDataTransferencia().isBefore(dataF))
+				.collect(Collectors.toList());
+	}
+	
+	private PageDTO<Transferencia> fazPaginas(int pageNumber, List<Transferencia> lista){
 		PagedListHolder<Transferencia> pageHolder = new PagedListHolder<>(lista);
 		pageHolder.setPageSize(4);
 		pageHolder.setPage(pageNumber);
@@ -59,14 +85,14 @@ public class TransferenciaService {
         long totalElements = pageHolder.getNrOfElements();
 
         PageRequest pageRequest = PageRequest.of(page, pageSize);
-        Page p = new PageImpl<>(elements, pageRequest, totalElements);
+        Page<Transferencia> p = new PageImpl<>(elements, pageRequest, totalElements);
         
         PageDTO<Transferencia> pdto = new PageDTO<>();
         pdto.setContent(p.getContent());
         pdto.setTotalPages(p.getTotalPages());
         pdto.setTotalElements(p.getTotalElements());
         
-        List<Transferencia> allTranferencias = listarTodasAsTransferencias();
+        List<Transferencia> allTranferencias = transferenciaRepository.findAll();
         BigDecimal somaTotal = allTranferencias.stream().map(t -> t.getValor()).reduce(BigDecimal.ZERO, BigDecimal::add);
         pdto.setSomaTotal(somaTotal);
         
@@ -76,7 +102,7 @@ public class TransferenciaService {
         return pdto;
 	}
 	
-	public LocalDateTime validaData(String data, String reg){
+	private LocalDateTime validaData(String data, String reg){
 		if(!data.matches(reg)) {
 			throw new IllegalArgumentException();
 		}
@@ -84,7 +110,7 @@ public class TransferenciaService {
 		return LocalDateTime.parse(data2 + " 00:00:00", DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
 	}
 	
-	public String validaNome(String nome, String reg){
+	private String validaNome(String nome, String reg){
 		if(!nome.matches(reg)) {
 			throw new IllegalArgumentException();
 		}
